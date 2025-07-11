636814966cfc3ae11bbbcbc02af6a004a1903bcf
andrecaly64 <105026467+andrecaly64@users.noreply.github.com>
2025-07-11 18:38:06 -0300

    Update proc.h preempcao

--- minix/kernel/proc.h
+++ minix/kernel/proc.h
@@ -138,7 +138,8 @@ struct proc {
 
 /* Definições para fila de prioridades estáticas */
 #define NUM_PRIORITIES 16  // Número de níveis de prioridade (0-15)
-#define QUEUE_SIZE_PER_PRIORITY 128  // Tamanho máximo de cada fila de prioridade
+#define QUEUE_SIZE_PER_PRIORITY 256  // Tamanho máximo de cada fila de prioridade
+#define PREEMPTION_TICKS 10  // Quantidade de ticks de clock antes de preemptar
 
 EXTERN struct proc *priority_queues[NUM_PRIORITIES][QUEUE_SIZE_PER_PRIORITY];
 EXTERN int queue_front[NUM_PRIORITIES];
